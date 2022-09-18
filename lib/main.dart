1a0482568f0a3621ba73fcda9d35a9a85406eb47
Amit Sharma <amitsharma63783@gmail.com>
2022-09-18 20:43:39 +0530

    day1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demoapp/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -10,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Material(
-        child: Center(
-          child: Text("This is my 30 days flutter tutorial"),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
