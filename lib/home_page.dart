cc6b4b2a24fed29be9daa03d9d918a47a378dc2a
Amit Sharma <amitsharma63783@gmail.com>
2022-09-19 19:31:51 +0530

    day 1 completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
 class HomePage extends StatelessWidget {
-  int days = 30;
-  String name = "Amit sharma";
+  final int days = 30;
+  final String name = "Amit sharma";
 
-  HomePage({super.key});
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
